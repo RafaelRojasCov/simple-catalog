7d5aff160fa105448e0921d3a8fae99692d7cfe1
Rafael Rojas <rafaelrojas.cov@gmail.com>
2019-10-27 22:16:55 -0300

    Add homepage with base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CatalogComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
